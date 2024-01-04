ba680dc46835bb755eb241c09a46e6853c99dae9
Ilhama Novruzova <inovruzova07@gmail.com>
2024-01-04 11:05:38 +0400

    Fixed HashRouter

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import React from 'react'
 import Home from './components/Home'
 import Cards from './components/Cards'
 import NoMatchRoute from './components/NoMatchRoute'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, HashRouter } from 'react-router-dom'
 import "./App.css"
 import ContactMe from './components/ContactMe'
 
 function App() {
   return (
     <div className="App">
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/cards" element={<Cards />} />
-      <Route path="/contact" element={<ContactMe />} />
-      <Route path="/*" element={<NoMatchRoute />} />
-    </Routes>
-  </div>
+      <HashRouter basename='/'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cards" element={<Cards />} />
+          <Route path="/contact" element={<ContactMe />} />
+          <Route path="/*" element={<NoMatchRoute />} />
+        </Routes>
+      </HashRouter>
+    </div>
   );
 }
 
